328712b3435c09c6507717ec03ad81e699474f52
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-04 13:09:52 -0600

    install webpack/babel dependencies and configure

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client')
+module.exports = {
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            preset: ['@babel/preset-env', '@babel/preset-rect']
+          }
+        }
+      }
+    ]
+  };
+  watch: true
+};
