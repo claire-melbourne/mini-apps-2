a8236f662233f6f5a87b423765da803efa97d32b
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-04 13:46:29 -0600

    write client-side search req via axios

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 function App() {
+  //write a get request which searches db by keyword(s?)
+
+  const search = (keyword) => {
+    axios.get(`http://localhost:3000/events?q=${keyword}`)
+    .then(res => console.log('RESPONSE: ', res))
+    .catch(err => console.error('ERROR: ', err))
+  }
   return (
-
     <div>Historical Records</div>
-
   );
 };
 
