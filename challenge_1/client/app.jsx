67fe82badd9ee76df5822934c48b4264475ac824
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-04 17:34:04 -0600

    store retrieved api data in state

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import Search from './Search.jsx'
+import Search from './Search.jsx';
+import SearchResults from './SearchResults.jsx';
 
 function App() {
-  //write a get request which searches db by keyword(s?)
+
+  const [data, setData] = useState([]);
 
   const searchRecordsByKeyword = (keyword) => {
     console.log(`Received ${keyword} for search`)
     axios.get(`http://localhost:3000/events?q=${keyword}`)
-    .then(res => console.log('RESPONSE: ', res))
+    .then(res => {
+      console.log('RESPONSE: ', res);
+      setData(res.data);
+    })
     .catch(err => console.error('ERROR: ', err))
   }
   return (
