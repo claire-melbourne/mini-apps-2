fdc803a5c21c742c5540c7e74a6c231c91527da4
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-04 19:26:27 -0600

    update get request and search results to start pagination process

--- challenge_1/client/SearchResults.jsx
+++ challenge_1/client/SearchResults.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactPaginate from 'react-paginate';
 import HistoricalRecord from './HistoricalRecord.jsx';
 
-function SearchResults({ data }) {
+function SearchResults({ data, pageCount }) {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageClick = (selectedPage) => {
+    setCurrentPage(selectedPage);
+  }
   if (data.length === 0) {
     return null;
   }
-  return (
 
+  return (
     <div>
+      <ReactPaginate
+        previousLabel= {'<-- Prev'}
+        nextLabel= {'Next -->'}
+        breakLabel={'...'}
+        pageCount= { pageCount }
+        onPageChange= { handlePageClick }
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        //don't know what the following do, but including for now for reference
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
       Search Results:
       {data.map(record => <HistoricalRecord record= {record} />)}
     </div>
--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -7,12 +7,18 @@ import SearchResults from './SearchResults.jsx';
 function App() {
 
   const [data, setData] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
 
   const searchRecordsByKeyword = (keyword) => {
     console.log(`Received ${keyword} for search`)
-    axios.get(`http://localhost:3000/events?q=${keyword}`)
+    axios.get(`http://localhost:3000/events?q=${keyword}&_limit=10&_page=1`)
     .then(res => {
       console.log('RESPONSE: ', res);
+      var splitLinks = ('' + res.headers.link).split('page=');
+      var lastPage = splitLinks[splitLinks.length - 1].split('>')[0];
+      setPageCount(lastPage);
+      //var lastPage = splitLinks[splitLinks.length]
+      console.log('RESPONSE LINKS Last page: ', lastPage)
       setData(res.data);
     })
     .catch(err => console.error('ERROR: ', err))
@@ -21,7 +27,7 @@ function App() {
     <div>
       <div>Find Historical Records</div>
       <Search searchRecordsByKeyword = { term => searchRecordsByKeyword(term) }/>
-      <SearchResults data= {data} />
+      <SearchResults data= {data} pageCount= {pageCount}/>
     </div>
   );
 };
