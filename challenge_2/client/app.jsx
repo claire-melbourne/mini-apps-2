d99f5d60f6c8c44600d9407ffe96bc32bfaae6b5
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-05 12:23:32 -0600

    set up webpack with basic app/index.html

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+function App() {
+  return (
+    <div>
+      Cryptocurrency Values Over Time
+      <div>
+      "Powered by CoinDesk"
+      </div>
+    </div>
+  )
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));
