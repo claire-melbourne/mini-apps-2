e201fd4de746dd863ca48a33724d9892f96fdb76
Claire Melbourne <claire.melbourne@gmail.com>
2020-11-05 12:32:04 -0600

    set up basic express server serving public dir

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const PORT = 5000;
+app = express();
 
+app.use(express.static(__dirname + '/public'))
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
+
+app.listen(PORT, () => { console.log(`Listening on ${PORT}`)})
 
